a8e67bf2e1b148c645d1a927f6a79205ef6145a3
Ofer Shaal <oshaal@phase2technology.com>
2022-02-07 00:57:06 -0500

    Support .diff extension as patch file

--- src/contentScripts/index.ts
+++ src/contentScripts/index.ts
@@ -36,7 +36,7 @@ function addButtons(patchLink: HTMLAnchorElement) {
 function findPatchesInPage() {
   const allLinks = document.querySelectorAll('a')
 
-  const patchesRegex = /.*\.patch$/
+  const patchesRegex = /.*\.(patch|diff)$/
   // find specific links in AllLinks
   const patchLinks = Array.from(allLinks).filter((link) => {
     return (patchesRegex.test(link.href) === true)
